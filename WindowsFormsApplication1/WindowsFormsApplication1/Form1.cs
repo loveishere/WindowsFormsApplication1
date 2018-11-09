2069dcc9f678e0568d78f84c878330a5a9249c61
wgb <wangguangbiao@live.com>
2018-11-09 15:19:30 +0800

    第一次提交

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text += "Hello World\r\n";
+            this.richTextBox1.SelectionStart = this.richTextBox1.TextLength;
+            this.richTextBox1.ScrollToCaret();
+        }
+    }
+}
